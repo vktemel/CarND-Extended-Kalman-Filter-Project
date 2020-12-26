603f8af50681f7fe2f76915c8dc380b8c67805da
vktemel <vktemel@umich.edu>
2020-12-26 18:35:53 -0500

    added rmse calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,6 +14,22 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4); 
+  rmse << 0, 0, 0, 0;
+  for(int i=0; i < estimations.size(); i++)
+  {
+     VectorXd residual = estimations[i]-ground_truth[i];
+
+     residual = residual.array()*residual.array();
+
+     rmse += residual;    
+  }
+  
+  rmse = rmse/estimations.size(); 
+  
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
