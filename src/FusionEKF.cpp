781f8c2dbeaf71b4a3d84bad19e8f43df2c32564
vktemel <vktemel@umich.edu>
2020-12-26 18:27:01 -0500

    added initialization for laser and radar measurements

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -49,6 +49,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   /**
    * Initialization
    */
+  cout << "processing measurement" << endl;
   if (!is_initialized_) {
     /**
      * TODO: Initialize the state ekf_.x_ with the first measurement.
@@ -61,18 +62,37 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
+    ekf_.P_ = MatrixXd(4,4);
+    ekf_.P_ << 1, 0, 0, 0,
+               0, 1, 0, 0, 
+               0, 0, 1000, 0,
+               0, 0, 0, 1000;
+    
+    float px, py;
+
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
       //         and initialize state.
+      float rho = measurement_pack.raw_measurements_[0];
+      float psi = measurement_pack.raw_measurements_[1];
+      float rhodot = measurement_pack.raw_measurements_[2]; 
 
+      cout << "rho: " << rho << ", and psi: " << psi << endl;
+
+      px = rho*cos(psi);
+      py = rho*sin(psi);
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.
-
+      px = measurement_pack.raw_measurements_[0];
+      py = measurement_pack.raw_measurements_[1];  
     }
-
+    
+    ekf_.x_ << px, py, 1, 1;
+    
     // done initializing, no need to predict or update
     is_initialized_ = true;
+    cout << "initialized, x_ is: " <<  ekf_.x_ << endl;
     return;
   }
 
@@ -86,8 +106,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * TODO: Update the process noise covariance matrix.
    * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
-
+  cout << "starting prediction" << endl;
   ekf_.Predict();
+  cout << "ended prediction" << endl;
 
   /**
    * Update
